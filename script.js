0c3e338a2750dd99b2015af88105075a7ac0233a
Suz <suzylee824@gmail.com>
2020-02-07 10:55:34 -0800

    update html

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+    $("#difficultyHeader").hide().fadeIn(500);
+    $(".buttonDiff").hide().fadeIn(1500);
 
-    $("#difficultiyHeader").fadeIn("slow")
+    $("#targetAreaHeader").hide().fadeIn(2000);
+    $(".firstRowBtn").hide().fadeIn(2500);
+    $(".secondRowBtn").hide().fadeIn(3000);
+
+    $("#musicAreaHeader").hide().fadeIn(3200);
+    $(".musicButtons").hide().fadeIn(3500);
+
+    $(".submitBtn").hide().fadeIn(4000);
+})
+    
 
